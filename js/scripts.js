d24a25a8dc80005320b8f5c969de950ce7aeca40
cb@design-busse.de <DTxhruKb5q>
2019-04-23 12:35:32 +0200

    Alert Data

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@
 
 window.addEventListener('message',function(event) {
    url = event.data;
+   alert(event.data);
    var ref = window.open(url, '_blank', 'location=yes,enableViewPortScale=yes');
 },false);
