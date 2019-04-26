e73014ac9d7057648b205db4356a804b1e44f176
cb@design-busse.de <DTxhruKb5q>
2019-04-26 07:57:04 +0200

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,6 @@ window.addEventListener('message',function(event) {
 	
 	url = event.data;
 	alert('message reseived');
-	var ref = window.open(url, '_blank', 'location=yes,enableViewPortScale=yes,Lage=no,SuppressesIncrementalRendering?=no');
+	var ref = window.open(url, '_blank', 'location=yes,enableViewPortScale=yes,Lage=no,SuppressesIncrementalRendering=no');
 	alert('message reseived');
 },false);
