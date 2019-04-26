00c5e13dcd8d106e06a442da1ff6714ddb8d100a
cb@design-busse.de <DTxhruKb5q>
2019-04-26 07:48:06 +0200

    Update Alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
 
 window.addEventListener('message',function(event) {
+	alert('message reseived');
    url = event.data;
    var ref = window.open(url, '_blank', 'location=yes,enableViewPortScale=yes');
 },false);
