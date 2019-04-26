fdecd98a20955c911ca178209086044e7dcd7a0e
cb@design-busse.de <DTxhruKb5q>
2019-04-26 08:32:52 +0200

    Debug Web

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@
 window.addEventListener('message',function(event) {
 	
 	url = event.data;
-	
+	var ref = window.open(encodeURI(jQuery(this).attr("data-link")), '_blank', 'location=no,enableViewPortScale=yes');
 	jQuery('#externalBrowser').attr("data-link", url);
+	alert(jQuery('#externalBrowser').attr("data-link"));
 	jQuery('#externalBrowser').click();
 },false);
